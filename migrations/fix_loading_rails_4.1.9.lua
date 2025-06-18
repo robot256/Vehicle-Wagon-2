9edf63c19a9e854439a0bdb54aff37ed5b54b447
robot256 <robot256@GMAIL.COM>
2025-06-18 00:18:13 -0400

    Fix bug in rail table migration, add logging

--- migrations/fix_loading_rails_4.1.9.lua
+++ migrations/fix_loading_rails_4.1.9.lua
@@ -1,6 +1,9 @@
 require("__VehicleWagon2__/script/makeGlobalMaps")
 
 -- Fix missing entries in storage.loading_rails due to bug that was present in 4.1.8 and earlier
+log("Before migration:")
+log("loading_rails=\n"..serpent.block(storage.loading_rails))
+log("unloading_rails=\n"..serpent.block(storage.unloading_rails))
 if storage.loading_ramps then
   storage.loading_rails = {}
   storage.unloading_rails = {}
@@ -8,17 +11,23 @@ if storage.loading_ramps then
     local ramp = entry.ramp
     if entry.loading_rail then
       local rail_unit_number = entry.loading_rail.unit_number
+      log("Ramp "..tostring(ramp_unit_number).." has a loading rail "..tostring(rail_unit_number))
       storage.loading_rails[rail_unit_number] = storage.loading_rails[rail_unit_number] or {}
       storage.loading_rails[rail_unit_number][ramp_unit_number] = ramp
     end
     if entry.unloading_rails then
-      for rail_unit_number,_ in pairs(storage.unloading_rails) do
+      log("Ramp "..tostring(ramp_unit_number).." has "..tostring(table_size(entry.unloading_rails)).." unloading rails.")
+      for rail_unit_number,_ in pairs(entry.unloading_rails) do
+        log("Ramp "..tostring(ramp_unit_number).." has a unloading rail "..tostring(rail_unit_number))
         storage.unloading_rails[rail_unit_number] = storage.unloading_rails[rail_unit_number] or {}
         storage.unloading_rails[rail_unit_number][ramp_unit_number] = ramp
       end
     end
   end
 end
+log("After migration:")
+log("loading_rails=\n"..serpent.block(storage.loading_rails))
+log("unloading_rails=\n"..serpent.block(storage.unloading_rails))
 
 -- Find existing manual trains
 storage.manual_trains = storage.manual_trains or {}
